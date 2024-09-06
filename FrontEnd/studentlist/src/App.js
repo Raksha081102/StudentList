b0b681975e59a207b1f563c4abeeecab57b7e9ce
unknown <raksharinky@gmail.com>
2024-09-06 14:44:36 +0530

    first commit

---
+++ FrontEnd/studentlist/src/App.js
@@ -0,0 +1,12 @@
+import StudentTable from './Components/StudentTable';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentTable/>
+    </div>
+  );
+}
+
+export default App;
